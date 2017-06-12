3a69915f16fcb30c1cfca0d544c36beb12084a85
dev-jip <herejip@gmail.com>
2017-06-12 21:20:03 +0900

    Delete db_config.py

--- static/db_config.py
+++
@@ -1,4 +0,0 @@
-db_config = {
-    'min_size': 2, 'max_size': 2, 'max_queries': 50000, 'host':'jipdb.ctcgmkl18wuj.ap-northeast-2.rds.amazonaws.com',
-    'user': 'jip', 'password': '35343534', 'database': 'love_story'
-  }
